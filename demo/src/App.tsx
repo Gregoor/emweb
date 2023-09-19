fce3a08d3b112aea13ba1ea5ecd110c23380fc75
Gregor Weber <mail@dflate.io>
2023-09-19 18:42:01 +0200

    fix(demo): non-local embeds

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -4,9 +4,8 @@ function App() {
   return (
     <div style={{ maxWidth: 600, margin: "0 auto" }}>
       <h1>Embeds</h1>
-      {/* <Embed url="https://powl.vercel.app/id/39k80qwag97grwvk4se5h2799jgy8s0" /> */}
-      <Embed url="http://localhost:3000/id/36xwsshad2hncw76hsadvj0r9jff0a8" />
-      {/* <Embed url="https://soundcloud.com/frida-darko/frida-darko-saga-bucht-2023" /> */}
+      <Embed url="https://powl.vercel.app/id/39k80qwag97grwvk4se5h2799jgy8s0" />
+      <Embed url="https://soundcloud.com/frida-darko/frida-darko-saga-bucht-2023" />
     </div>
   );
 }
