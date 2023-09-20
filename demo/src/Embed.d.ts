18c1a95d4b13f7bb20fe96527dbf9fe2289fc103
Gregor Weber <mail@dflate.io>
2023-09-20 13:01:37 +0200

    chore: fix demo build

--- demo/src/Embed.d.ts
+++
@@ -1,9 +0,0 @@
-type Props = {
-  url: string;
-  maxHeight?: number;
-  maxWidth?: number;
-};
-export declare function Embed(
-  props: Props,
-): import("react/jsx-runtime").JSX.Element | null;
-export {};
